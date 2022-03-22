5a122b8a978d327e53d6c49c9948fc389dc0a059
nealcaffrey <1434376870@qq.com>
2022-03-22 23:00:12 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,55 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    total:0,
+    list:[],
+    knowledge:[]
+  },
+  // 事件处理函数
+  onLoad() {
+    wx.request({
+      url: 'https://liuchuanqi.com/api/interview/category',
+      header:{
+        'content-type':'application/json'
+      },
+      success: res => {
+        console.log(res.data)
+        this.setData({
+          total:res.data.total,
+          list:res.data.list
+        })
+      }
+    })
+  },
+  //点击分类
+  showItem:function (event) {
+    //当前点击的分类ID
+    let categoryId = event.currentTarget.dataset.id;
+    //判断分类状态
+    var data = this.data.list;
+    for (var i=0; i<data.length;i++) {
+      if (data[i].id == categoryId) {
+        data[i].show = data[i].show ? false : true;
+      } else {
+        data[i].show = false;
+      }
+    }
+    this.setData({
+      list:data
+    })
+    wx.request({
+      url: 'https://liuchuanqi.com/api/interview/knowledge?category_id='+categoryId,
+      header:{
+        'content-type':'application/json'
+      },
+      success: res => {
+        this.setData({
+          knowledge:res.data.list
+        })
+      }
+    })
+  }
+})
